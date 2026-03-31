e9133ef48c744188d3dca8da331aa41421d036fa
Akshay Atanure <akshayatanure11@gmail.com>
2026-04-01 00:55:37 +0530

    Update decision.py

--- core/decision.py
+++ core/decision.py
@@ -1,17 +1,22 @@
 def get_decision(prob, low, high):
+
     if prob <= low:
-        return (
-            "The borrower demonstrates a strong financial profile with stable income, a high credit score, and a low debt burden. Default risk is low.\n\n\
-            ✅ Decision: Approve."
-        )
+        return """The borrower demonstrates a strong financial profile with stable income, 
+a high credit score, and a low debt burden.
+Default risk is low.
+
+✅ Decision: Approve"""
 
     elif low < prob <= high:
-        return (
-            "The borrower shows moderate risk with some inconsistencies in financial behavior and a slightly higher debt burden.\n\Default risk is manageable but requires further assessment.
-            \n\⚠️ Decision: Review (Manual assessment recommended).")
+        return """The borrower shows moderate risk with some inconsistencies in financial behavior 
+and a slightly higher debt burden.
+Default risk is manageable but requires further assessment.
+
+⚠️ Decision: Review (Manual assessment recommended)"""
 
     else:
-        return (
-            "The borrower exhibits high default risk, characterized by weaker financial indicators such as high debt burden or lower credit quality.\n\n\
-            ❌ Decision: Reject."
-        )
+        return """The borrower exhibits high default risk, characterized by weaker financial indicators 
+such as high debt burden or lower credit quality.
+Default risk is high.
+
+❌ Decision: Reject"""
