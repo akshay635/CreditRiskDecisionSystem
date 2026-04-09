06b033a3611e1cf7ce87813dc71c1588c78b1dbc
Akshay Atanure <akshayatanure11@gmail.com>
2026-04-09 10:26:52 +0530

    Update page2.py

--- app_pages/page2.py
+++ app_pages/page2.py
@@ -8,7 +8,7 @@ importlib.reload(config)
 from sklearn.metrics import roc_auc_score, average_precision_score, accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 
 def BatchwisePrediction():
-  st.title("Batchwise Portfolio Risk Analysis")
+  st.title("Batchwise Default Risk Analysis")
 
   uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
 
