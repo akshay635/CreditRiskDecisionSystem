234ff4549df4cd8cd6f9a4653a2a261a17ea2126
Akshay Atanure <akshayatanure11@gmail.com>
2026-04-09 09:59:09 +0530

    Update page2.py

--- app_pages/page2.py
+++ app_pages/page2.py
@@ -1,1 +1,11 @@
 
+import streamlit as st
+import pandas as pd
+import numpy as np
+import importlib
+import training.config as config
+importlib.reload(config)
+from sklearn.metrics import roc_auc_score, average_precision_score, accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+
+def BatchWiseAssessment():
+  st.title("Batchwise Portfolio Risk Analysis")
