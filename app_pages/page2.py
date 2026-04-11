4303cde7dbd9f7b0ca63115d2bafcd5c104167de
Akshay Atanure <akshayatanure11@gmail.com>
2026-04-11 13:25:35 +0530

    Update page2.py

--- app_pages/page2.py
+++ app_pages/page2.py
@@ -108,9 +108,14 @@ def BatchwisePrediction():
     fp_cost = avg_loan_amount_nd * avg_interest
     fn_cost = avg_loan_amount_d + (avg_loan_amount_d*avg_interest)
 
-    expected_loss = fp_cost*fp + fn_cost*fn
+    npas = fn*fn_cost
+    opportunity_cost = fp*fp_cost
+    total_expected_loss = fp_cost*fp + fn_cost*fn
     
-    st.error(f'Expected_loss: {round(expected_loss)}/-')
+    col12, col13, col14 = st.columns(3)
+    col12.metric('Total Expected Loss', f"{round(total_expected_loss)}/-", delta_color='yellow')
+    col13.metric('Opportunity Cost', f"{round(opportunity_cost}/-", delta_color='orange')
+    col14.metric('NPAs (Non-Performing Assets)', f"{round(npas)}/-", delta_color='red'
 
     new_df["Risk Bucket"] = pd.cut(probabilities, bins=[0, 0.3, 0.6, 1],
                                    labels=["Low Risk", "Medium Risk", "High Risk"])
