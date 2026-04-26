ba23715c57a9f65f0bc8972ff69f25106fb605b5
Akshay Atanure <akshayatanure11@gmail.com>
2026-04-26 20:38:30 +0530

    Update page2.py

--- app_pages/page2.py
+++ app_pages/page2.py
@@ -82,8 +82,8 @@ def compute_business_metrics(df, probs, preds, recovery_rate, ccf):
     df['EAD'] = df['CurrentBalance'] + (df['TotalCreditLimit'] - df['CurrentBalance']) * ccf
 
     df['ExpectedLoss'] = df['Probabilities'] * df['LGD'] * df['EAD']
-    df['WeightedPD'] = (probs*df['EAD'])/(df['EAD'].sum())                                             
-
+    df['WeightedPD'] = (df['Probabilities']*df['EAD'])/(df['EAD'].sum())
+    weightedPD = df['WeightedPD'].mean()
     # Cost calculation
     avg_loan_d = df[df['LoanDefault'] == 1]['LoanAmount'].mean()
     avg_loan_nd = df[df['LoanDefault'] == 0]['LoanAmount'].mean()
@@ -98,7 +98,7 @@ def compute_business_metrics(df, probs, preds, recovery_rate, ccf):
         "df": df,
         "npas": fn * fn_cost,
         "opportunity_cost": fp * fp_cost,
-        "weighted_pd": df['WeightedPD'].mean(),
+        "weighted_pd": weightedPD,
         "avg_pd": df['Probabilities'].mean(),
         "avg_lgd": df['LGD'].mean(),
         "avg_ead": df['EAD'].mean(),
