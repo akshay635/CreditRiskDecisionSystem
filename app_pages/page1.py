3aca8846c3f7ec4d62c7b25b27d66139e067e79a
Akshay Atanure <akshayatanure11@gmail.com>
2026-04-27 11:44:50 +0530

    Update page1.py

--- app_pages/page1.py
+++ app_pages/page1.py
@@ -104,11 +104,6 @@ def RiskAssessment():
     if loan_amount <= 0:
         st.error("⚠️ Loan amount must be greater than 0")
         st.stop()
-
-    override = st.checkbox("Override LGD?")
-
-    if override:
-        lgd = st.slider("Adjust LGD", 0.0, 1.0, 0.5)
         
     # Predict
     if st.button("Predict"):
